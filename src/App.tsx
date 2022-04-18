c18aed994511dab1d46a9b548c0e3162999ebcd5
Bianca-sa <bianca_desa@outlook.com>
2022-04-18 20:27:14 -0300

    Construindo homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import { Home } from './pages/Home';
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <a href=''></a>
-    </>
+    <Home />
   );
 }
 
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,18 +1,20 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: 'letmeask-b8d74.firebaseapp.com',
-  databaseURL: 'https://letmeask-b8d74-default-rtdb.firebaseio.com',
-  projectId: 'letmeask-b8d74',
-  storageBucket: 'letmeask-b8d74.appspot.com',
-  messagingSenderId: '830225750768',
-  appId: '1:830225750768:web:568af2cfc6c6883d5b4035',
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+ const auth = firebase.auth()
+ const database = firebase.firestore();
+
+export { firebase, auth, database }
